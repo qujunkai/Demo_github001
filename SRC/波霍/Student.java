cb12a9f20cdbe0cc051adf5c194f41c8d1e80684
qujunkai <66863534+qujunkai@users.noreply.github.com>
2020-06-13 18:48:12 +0800

    Update Student.java

--- SRC/波霍/Student.java
+++ SRC/波霍/Student.java
@@ -3,8 +3,9 @@ package pojo;
 public class Student {
 
     private int id;
-    private String name;
+   // private String name;
     
     private String name//姓名kaikai
+        private String sex;
 
 }
